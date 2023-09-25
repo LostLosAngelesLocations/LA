8742d96f314fd2d15190a79cf22eb7bac7f11252
Admin <145495101+LostLosAngelesLocations@users.noreply.github.com>
2023-09-25 14:14:21 -0700

    update list

--- imgs.php
+++ imgs.php
@@ -1,3 +1,4 @@
+#!/usr/local/bin/php
 <?php
 $curDir = dirname(__FILE__);
 $images = glob($curDir . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . 'art' . DIRECTORY_SEPARATOR . '*.{jpeg,jpg,gif,png}', GLOB_BRACE);
@@ -6,4 +7,4 @@ foreach ($images as $key => $val) {
     $images[$key] = str_replace($curDir . DIRECTORY_SEPARATOR, '', $val);
 }
 
-echo json_encode($images);
+file_put_contents($curDir . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . 'art' . DIRECTORY_SEPARATOR . 'list.json', json_encode($images));
