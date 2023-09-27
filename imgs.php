754b28acee467e581a8fe7a6542057b6d6c3b1b7
Admin <145495101+LostLosAngelesLocations@users.noreply.github.com>
2023-09-27 13:59:19 -0700

    revisions

--- imgs.php
+++ imgs.php
@@ -3,6 +3,8 @@
 $curDir = dirname(__FILE__);
 $images = glob($curDir . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . 'art' . DIRECTORY_SEPARATOR . '*.{jpeg,jpg,gif,png}', GLOB_BRACE);
 
+natsort($images);
+
 foreach ($images as $key => $val) {
     $images[$key] = str_replace($curDir . DIRECTORY_SEPARATOR, '', $val);
 }
